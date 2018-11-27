033250249b4dd7e5f56fb5ac5e623c88e30429ff
pavlushk0 <pavlushk0@mail.ru>
2018-11-27 21:54:17 +0400

    DT

--- mtrx34.cpp
+++ mtrx34.cpp
@@ -330,12 +330,12 @@ mtrx3_t	mtrx_invert(const mtrx3_t &m) {
 	mtrx3_t inverse, rt;
 	float det, invDet;
 
-	inverse[id_rw(0, 0, 3)] = m[id_rw(1, 1, 3)] * m[id_rw(2, 2, 3)] - m[id_rw(1, 2, 3)] * m[id_rw(2, 1, 3)];
-	inverse[id_rw(1, 0, 3)] = m[id_rw(1, 2, 3)] * m[id_rw(2, 0, 3)] - m[id_rw(1, 0, 3)] * m[id_rw(2, 2, 3)];
-	inverse[id_rw(2, 0, 3)] = m[id_rw(1, 0, 3)] * m[id_rw(2, 1, 3)] - m[id_rw(1, 1, 3)] * m[id_rw(2, 0, 3)];
+	inverse[0] = m[4] * m[8] - m[5] * m[7];
+	inverse[3] = m[5] * m[6] - m[3] * m[8];
+	inverse[6] = m[3] * m[7] - m[4] * m[6];
 
-	det = m[id_rw(0, 0, 3)] * inverse[id_rw(0, 0, 3)] + m[id_rw(0, 1, 3)] * inverse[id_rw(1, 0, 3)] + 
-		  m[id_rw(0, 2, 3)] * inverse[id_rw(2, 0, 3)];
+	det = m[0] * inverse[0] + m[1] * inverse[3] + 
+		  m[2] * inverse[6];
 
 	if (fabs(det) < f_eps) {
 		return mtrx3_t();
@@ -343,24 +343,24 @@ mtrx3_t	mtrx_invert(const mtrx3_t &m) {
 
 	invDet = 1.0f / det;
 
-	inverse[id_rw(0, 1, 3)] = m[id_rw(0, 2, 3)] * m[id_rw(2, 1, 3)] - m[id_rw(0, 1, 3)] * m[id_rw(2, 2, 3)];
-	inverse[id_rw(0, 2, 3)] = m[id_rw(0, 1, 3)] * m[id_rw(1, 2, 3)] - m[id_rw(0, 2, 3)] * m[id_rw(1, 1, 3)];
-	inverse[id_rw(1, 1, 3)] = m[id_rw(0, 0, 3)] * m[id_rw(2, 2, 3)] - m[id_rw(0, 2, 3)] * m[id_rw(2, 0, 3)];
-	inverse[id_rw(1, 2, 3)] = m[id_rw(0, 2, 3)] * m[id_rw(1, 0, 3)] - m[id_rw(0, 0, 3)] * m[id_rw(1, 2, 3)];
-	inverse[id_rw(2, 1, 3)] = m[id_rw(0, 1, 3)] * m[id_rw(2, 0, 3)] - m[id_rw(0, 0, 3)] * m[id_rw(2, 1, 3)];
-	inverse[id_rw(2, 2, 3)] = m[id_rw(0, 0, 3)] * m[id_rw(1, 1, 3)] - m[id_rw(0, 1, 3)] * m[id_rw(1, 0, 3)];
+	inverse[1] = m[2] * m[7] - m[1] * m[8];
+	inverse[2] = m[1] * m[5] - m[2] * m[4];
+	inverse[4] = m[0] * m[8] - m[2] * m[6];
+	inverse[5] = m[2] * m[3] - m[0] * m[5];
+	inverse[7] = m[1] * m[6] - m[0] * m[7];
+	inverse[8] = m[0] * m[4] - m[1] * m[3];
 
-	rt[id_rw(0, 0, 3)] = inverse[id_rw(0, 0, 3)] * invDet;
-	rt[id_rw(0, 1, 3)] = inverse[id_rw(0, 1, 3)] * invDet;
-	rt[id_rw(0, 2, 3)] = inverse[id_rw(0, 2, 3)] * invDet;
+	rt[0] = inverse[0] * invDet;
+	rt[1] = inverse[1] * invDet;
+	rt[2] = inverse[2] * invDet;
 
-	rt[id_rw(1, 0, 3)] = inverse[id_rw(1, 0, 3)] * invDet;
-	rt[id_rw(1, 1, 3)] = inverse[id_rw(1, 1, 3)] * invDet;
-	rt[id_rw(1, 2, 3)] = inverse[id_rw(1, 2, 3)] * invDet;
+	rt[3] = inverse[3] * invDet;
+	rt[4] = inverse[4] * invDet;
+	rt[5] = inverse[5] * invDet;
 
-	rt[id_rw(2, 0, 3)] = inverse[id_rw(2, 0, 3)] * invDet;
-	rt[id_rw(2, 1, 3)] = inverse[id_rw(2, 1, 3)] * invDet;
-	rt[id_rw(2, 2, 3)] = inverse[id_rw(2, 2, 3)] * invDet;
+	rt[6] = inverse[6] * invDet;
+	rt[7] = inverse[7] * invDet;
+	rt[8] = inverse[8] * invDet;
 
 	return rt;
 }
