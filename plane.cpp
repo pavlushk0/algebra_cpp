360eaf55a74c8a9e7b68eeaae83551ab79dcf29a
pavlushk0 <pavlushk0@mail.ru>
2018-12-14 17:50:28 +0400

    DT win commit

--- plane.cpp
+++ plane.cpp
@@ -13,7 +13,7 @@ vec3_t plane_ray_insct(const vec3_t &rs, const vec3_t &re) {
 	return vec3_t();
 }
 
-bool	point_in_trngl(const vec3_t &v1, 
+bool point_in_trngl(const vec3_t &v1, 
 					   const vec3_t &v2, 
 					   const vec3_t &v3,
 					   const vec3_t &point) {
